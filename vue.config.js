7971db5976c720a2802ebde0048a3b75710888e6
anzihan12514 <anzihan12514@ipalfish.com>
2025-09-11 20:20:34 +0800

    进行mock数据模拟

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,5 @@
+const data = require('./data.json')
+
 module.exports = {
   css: {
     loaderOptions: {
@@ -13,6 +15,28 @@ module.exports = {
     'cube-ui': {
       postCompile: true,
       theme: true
+    }
+  },
+  devServer: {
+    before: app => {
+      app.get('/api/seller', (req, res) => {
+        res.json({
+          errno: 0,
+          data: data.seller
+        })
+      })
+      app.get('/api/goods', (req, res) => {
+        res.json({
+          errno: 0,
+          data: data.goods
+        })
+      })
+      app.get('/api/ratings', (req, res) => {
+        res.json({
+          errno: 0,
+          data: data.ratings
+        })
+      })
     }
   }
 }
